f823b051950dec5d931278a543259ffd545d9b3c
(Jiyoon) <(jiyooon1118@gmail.com)>
2023-07-10 16:04:37 +0900

    Feat: 파일수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,13 @@ cd $REPOSITORY
 echo "> build 파일 복사"
 cp $REPOSITORY/$PROJECT_NAME/server/build/libs/server-0.0.1-SNAPSHOT.jar $REPOSITORY/
 
+echo "> 삭제할 파일 확인 및 삭제"
+FILE_TO_DELETE=$REPOSITORY/$PROJECT_NAME/server/gradlew.bat
+if [ -f $FILE_TO_DELETE ]; then
+  echo "> $FILE_TO_DELETE 파일이 존재하여 삭제합니다."
+  rm $FILE_TO_DELETE
+fi
+
 echo "> 현재 실행중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
@@ -31,15 +38,6 @@ fi
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -t $REPOSITORY/*.jar | head -n 1)
-
-if [ -f "$JAR_FILE" ]; then
-  JAR_NAME=$(basename "$JAR_FILE")
-  echo "> Jar Name: $JAR_NAME"
-  nohup java -jar "$REPOSITORY/$JAR_NAME" 2>&1 &
-else
-  echo "> Error: JAR file not found in $REPOSITORY"
-  exit 1
-fi
 
 echo "> Jar Name: $JAR_NAME"
 nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
