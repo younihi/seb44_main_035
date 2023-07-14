7d3162455e6bdbb5a15bc8e73e1bb89952896261
(Jiyoon) <(jiyooon1118@gmail.com)>
2023-07-14 16:20:17 +0900

    feat: comment CRUD구현

--- server/src/main/java/com/server/server/global/exception/ExceptionCode.java
+++ server/src/main/java/com/server/server/global/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
     RECIPE_NOT_FOUND(404, "Recipe not found"),
-    RECOMMEND_NOT_FOUND(404, "Recommend not found");
+    RECOMMEND_NOT_FOUND(404, "Recommend not found"),
+    COMMENT_NOT_FOUND(404, "해당 댓글을 찾을 수 없습니다.");
 
     @Getter
     private int status;
