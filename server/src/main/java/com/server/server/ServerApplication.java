1724d25414d513a8ba721eec508f9ad2cb3aa00a
(Jiyoon) <(jiyooon1118@gmail.com)>
2023-07-23 13:35:45 +0900

    fix : 전체 삭제3

--- server/src/main/java/com/server/server/ServerApplication.java
+++ server/src/main/java/com/server/server/ServerApplication.java
@@ -2,9 +2,14 @@ package com.server.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServerApplication {
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
